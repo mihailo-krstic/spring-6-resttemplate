21330622498e98c1ba14ba9c1e1e0ba85e4bba63
Mihailo Krstic <mihailo.krstic@eng.it>
2026-04-23 16:56:44 +0200

    RestTemplateBuilder configuration - base url

--- src/main/java/guru/springframework/spring7resttemplate/config/RestTemplateBuilderConfig.java
+++ src/main/java/guru/springframework/spring7resttemplate/config/RestTemplateBuilderConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.restclient.RestTemplateBuilder;
 import org.springframework.boot.restclient.autoconfigure.RestTemplateBuilderConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
 import java.time.Duration;
 
@@ -12,8 +13,16 @@ public class RestTemplateBuilderConfig {
 
     @Bean
     RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer) {
-        return configurer.configure(new RestTemplateBuilder())
+
+        RestTemplateBuilder builder = configurer.configure(new RestTemplateBuilder())
                 .connectTimeout(Duration.ofSeconds(5))
                 .readTimeout(Duration.ofSeconds(2));
+
+        DefaultUriBuilderFactory uriBuilderFactory = new
+                DefaultUriBuilderFactory("http://localhost:8080");
+
+
+        return builder.uriTemplateHandler(uriBuilderFactory);
+
     }
 }
